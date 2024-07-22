4463efb9ca623ee8a1cbcc7bdcf7538999179a8d
tsummey <50956158+tsummey@users.noreply.github.com>
2024-07-22 07:33:28 -0400

    Update get-videos.ps1

--- vision/06-video-indexer/get-videos.ps1
+++ vision/06-video-indexer/get-videos.ps1
@@ -1,5 +1,5 @@
-$account_id="5568f35a-1458-4690-a848-b423041f65de"
-$api_key="1f250196b07f4791b976b44c383a08ee"
+$account_id="<YOUR ACCOUNT ID"
+$api_key="YOUR KEY"
 $location="trial"
 
 # Call the AccessToken method with the API key in the header to get an access token
