cab34ef18ca09d52f5c4893a783e1a7fd4b82750
tsummey <50956158+tsummey@users.noreply.github.com>
2024-07-22 07:32:30 -0400

    Add files via upload

---
+++ vision/06-video-indexer/get-videos.ps1
@@ -0,0 +1,9 @@
+$account_id="5568f35a-1458-4690-a848-b423041f65de"
+$api_key="1f250196b07f4791b976b44c383a08ee"
+$location="trial"
+
+# Call the AccessToken method with the API key in the header to get an access token
+$token = Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/auth/$location/Accounts/$account_id/AccessToken" -Headers @{'Ocp-Apim-Subscription-Key' = $api_key}
+
+# Use the access token to make an authenticated call to the Videos method to get a list of videos in the account
+Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/$location/Accounts/$account_id/Videos?accessToken=$token" | ConvertTo-Json -Depth 6
