d0a383047906b05e12945f26ff986a9020d97b21
tsummey <50956158+tsummey@users.noreply.github.com>
2024-07-30 09:06:23 -0400

    Update replace.ps1

--- vision/02-image-classification/replace.ps1
+++ vision/02-image-classification/replace.ps1
@@ -1,2 +1,2 @@
-$storageAcct = '<YOUR STORAGE ACCOUNT NAME'
+$storageAcct = '<YOUR STORAGE ACCOUNT NAME>'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
